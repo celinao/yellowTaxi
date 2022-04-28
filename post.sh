ff0e77839983b34b1a8067a27d8e529578cbc1f1
connorweida <cweida@wisc.edu>
2022-04-28 16:26:18 -0500

    second commit on post.sh, trying to get the average of all of four columns in the combined coefs.csv file that this script writes

--- post.sh
+++ post.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # combine the coef files
-sort -k1,1 -g -r coef*.csv > combine.csv
+cat regression_coefs/coef_*.csv > combinecoefs.csv
 
 # take the mean of the combined file and write it to a smaller file
-awk  '{ total += $1; count++ } END { print total/count }' combine.csv > smallcombine.csv
-awk '{total += $2; count++ } END { print total/count }' combine.csv >> smallcombine.csv
+cat combinecoefs.csv | { sum=0; while read $1; do sum=$(($sum + $1)); done; } | bc > smallcombinecoefs.csv
+
+#awk  '{ total += $1; count++ } END { print total/count }' combine.csv > smallcombine.csv
+#awk '{total += $2; count++ } END { print total/count }' combine.csv >> smallcombine.csv
+#awk '{total += $3; count++ } END { print total/count }' combine.csv >> smallcombine.csv
+#awk '{total += $4; count++ } END { print total/count }' combine.csv >> smallcombine.csv
 
 # call R on the smaller file
