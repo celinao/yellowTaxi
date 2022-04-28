952eff08ebc7bf52592c893d4bbdeaa4361a5aeb
connorweida <cweida@wisc.edu>
2022-04-28 18:44:56 -0500

    Next post commit, just need to call R file on smallcombinecoefs.csv

--- post.sh
+++ post.sh
@@ -4,11 +4,16 @@
 cat regression_coefs/coef_*.csv > combinecoefs.csv
 
 # take the mean of the combined file and write it to a smaller file
-cat combinecoefs.csv | { sum=0; while read $1; do sum=$(($sum + $1)); done; } | bc > smallcombinecoefs.csv
+cat combinecoefs.csv | 
+cut -d , -f 1 | awk  '{ total += $1; count++ } END { print total/count }' > smallcombinecoefs.csv
 
-#awk  '{ total += $1; count++ } END { print total/count }' combine.csv > smallcombine.csv
-#awk '{total += $2; count++ } END { print total/count }' combine.csv >> smallcombine.csv
-#awk '{total += $3; count++ } END { print total/count }' combine.csv >> smallcombine.csv
-#awk '{total += $4; count++ } END { print total/count }' combine.csv >> smallcombine.csv
+cat combinecoefs.csv |
+cut -d , -f 2 | awk  '{ total += $1; count++ } END { print total/count }' >> smallcombinecoefs.csv
 
-# call R on the smaller file
+cat combinecoefs.csv |
+cut -d , -f 3 | awk  '{ total += $1; count++ } END { print total/count }' >> smallcombinecoefs.csv
+
+cat combinecoefs.csv |
+cut -d , -f 4 | awk  '{ total += $1; count++ } END { print total/count }' >> smallcombinecoefs.csv
+
+# call R on the smaller file named smallcombinecoefs.csv
